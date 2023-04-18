31f3aa64c51809640a1c6ca9b6c27ca63974e9d5
Jo-Scar <joscarproductions@gmail.com>
2023-04-18 15:27:53 -0400

    InitialCommit

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 console.log("hello does this work");
-let list = document.querySelector("#cats-list");
+let list = document.querySelector("#shopping-list");
 let input = document.querySelector("#input");
-function addCat() {
-    let cat = input.value;
-    if (cat === "") {
+function addToShoppingList() {
+    let shoppingList = input.value;
+    if (shoppingList === "") {
         alert("This Field is Required");
     }
     else {
-        list.innerHTML += `<li>${cat}</li>`;
+        list.innerHTML += `<li>${shoppingList}</li>`;
+        input.value = "";
+    }
+}
+
+// Remove Item from Shopping List 
+
+function removeFromShoppingList() {
+    let shoppingList = input.value;
+    if (shoppingList === "") {
+        list.innerHTML -= `<li>${shoppingList}</li>`;
+        input.value = "";
+    }
+    else {
+        alert("This Field is Required");
         input.value = "";
     }
 }
